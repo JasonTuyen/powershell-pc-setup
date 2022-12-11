600737073772b0551cefb7e5aba302ec56fcccdf
Jason Tuyen <jtuyen99@gmail.com>
2022-12-11 13:21:13 -0800

    added powershell script

---
+++ software.ps1
@@ -0,0 +1,47 @@
+#Copied from: https://chris-ayers.com/2021/08/01/scripting-winget/
+#Install WinGet
+$hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
+if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
+    "Installing winget Dependencies"
+    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+
+    $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $releases = Invoke-RestMethod -uri $releases_url
+    $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
+
+    "Installing winget from $($latestRelease.browser_download_url)"
+    Add-AppxPackage -Path $latestRelease.browser_download_url
+}
+else {
+    "winget already installed"
+}
+
+#Install needed apps
+$apps = @(
+    @{name = "Microsoft.VisualStudioCode" },
+    @{name = "Microsoft.PowerToys" },
+    @{name = "Git.Git" },
+    @{name = "Oracle.VirtualBox" },
+    @{name = "Discord.Discord" },
+    @{name = "Valve.Steam" },
+    @{name = "Spotify.Spotify" }
+);
+
+Foreach ($app in $apps) {
+    #check if the app is already installed
+    $listApp = winget list --exact -q $app.name
+    if (![String]::Join("", $listApp).Contains($app.name)) {
+        Write-host "Installing:" $app.name
+        if ($app.source -ne $null) {
+            winget install --exact --silent $app.name --source $app.source
+        }
+        else {
+            winget install --exact --silent $app.name 
+        }
+    }
+    else {
+        Write-host "Skipping Install of " $app.name
+    }
+}
